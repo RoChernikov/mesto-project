94ae4d00f6b911949fafaf4ef8aa679e35c33e76
Ro Chernikov <imrhubris@gmail.com>
2021-11-06 16:14:55 +0300

    ref: переписал работу лайков, сократил код, убрал сильную зависимость от API, переписал функцию разворачивания контейнера лайков на промис

--- src/components/Сard.js
+++ src/components/Сard.js
@@ -6,7 +6,7 @@ export default class Card {
     { likes, link, name, owner, _id: id },
     currentUserId,
     templateSelector,
-    { handleCardClick, handleCardDelete }
+    { handleCardClick, handleCardDelete, handleLikeClick }
   ) {
     this._name = name;
     this._link = link;
@@ -17,6 +17,7 @@ export default class Card {
     this._currentUserId = currentUserId;
     this._handleCardClick = handleCardClick;
     this._handleCardDelete = handleCardDelete;
+    this._handleLikeClick = handleLikeClick;
   }
 
   _getTemplate() {
@@ -28,26 +29,96 @@ export default class Card {
     return cardElement;
   }
 
-  _setTrashBtnState() {
-    if (this._ownerId === this._currentUserId) {
-      this._card
-        .querySelector('.cards__trash-btn')
-        .classList.add('cards__trash-btn_visible');
+  generateCard() {
+    this._card = this._getTemplate();
+    this._cardImage = this._card.querySelector('.cards__image');
+    this._cardHeading = this._card.querySelector('.cards__heading');
+    this._likeCounter = this._card.querySelector('.cards__like-counter');
+    this._likeBtn = this._card.querySelector('.cards__like-btn');
+    this._trashBtn = this._card.querySelector('.cards__trash-btn');
+    this._spinner = this._card.querySelector('.cards__spinner');
+    const imageLoader = new ImageLoader(
+      this._cardImage,
+      this._spinner,
+      'cards__image_error'
+    );
+    this._cardImage.src = this._link;
+    this._cardImage.alt = this._name;
+    this._cardHeading.textContent = this._name;
+    this._likeCounter.textContent = this._likes.length;
+    this._setTrashBtnState();
+    this._setLikeState();
+    this._setEventListeners();
+    imageLoader.initialize(); // <----------------------------------------------------------ПЕРЕДЕЛАТЬ
+    return this._card;
+  }
+
+  deleteCard() {
+    this._card.remove();
+    this._card = null;
+  }
+
+  isLiked() {
+    return this._isLiked;
+  }
+
+  setLike(data) {
+    this._isLiked =
+      data.likes.filter(item => {
+        return item._id == this._currentUserId;
+      }).length > 0;
+    this._likeCounter.textContent = data.likes.length;
+    if (this._isLiked) {
+      this._likeBtn.classList.add('cards__like-btn_active');
+    } else {
+      this._likeBtn.classList.remove('cards__like-btn_active');
+    }
+    this._toggleLikeContainer(data);
+  }
+
+  _toggleLikeContainer(data) {
+    if (data.likes.length === 1)
+      this._likeCounter.classList.add('cards__like-counter_active');
+    else if (data.likes.length === 0) {
+      this._likeCounter.classList.remove('cards__like-counter_active');
     }
   }
 
-  _initializeLikeState() {
-    if (this._likes.length > 0)
-      this._card
-        .querySelector('.cards__like-counter')
-        .classList.add('cards__like-counter_active');
-    if (this._likes.some(item => item._id === this._currentUserId))
-      this._card
-        .querySelector('.cards__like-btn')
-        .classList.toggle('cards__like-btn_active');
+  _setTrashBtnState() {
+    if (this._ownerId === this._currentUserId) {
+      this._trashBtn.classList.add('cards__trash-btn_visible');
+    }
   }
-  // ----------------------------------------------------Функционал лайков
 
+  _setLikeState() {
+    if (this._likes.length > 0) {
+      this._likeCounter.classList.add('cards__like-counter_active');
+    }
+    if (this._likes.some(item => item._id === this._currentUserId)) {
+      this._likeBtn.classList.add('cards__like-btn_active');
+      this._isLiked = true;
+    }
+  }
+
+  _setEventListeners() {
+    this._card
+      .querySelector('.cards__trash-btn')
+      .addEventListener('click', () => this._handleCardDelete());
+    this._card
+      .querySelector('.cards__like-btn')
+      .addEventListener('click', () => this._handleLikeClick());
+    this._card.querySelector('.cards__image').addEventListener('click', () => {
+      this._handleCardClick({ name: this._name, link: this._link });
+    });
+  }
+}
+
+//**********************************************************************************************************************************************
+//**********************************************************************************************************************************************
+//**********************************************************************************************************************************************
+//**********************************************************************************************************************************************
+// ----------------------------------------------------Функционал лайков
+/*
   //---+++++Сворачивает контейнер лайков+++++---
   _closeLikeContainer() {
     this._likeCounter.classList.remove('cards__like-counter_active');
@@ -73,7 +144,7 @@ export default class Card {
         this._setLikeCounter(data);
       })
       .catch(err => {
-        console.log(err); 
+        console.log(err);
       });
   }
 
@@ -86,7 +157,7 @@ export default class Card {
         this._setLikeCounter(data);
       })
       .catch(err => {
-        console.log(err); 
+        console.log(err);
       });
   }
 
@@ -111,44 +182,8 @@ export default class Card {
     }
     this._toggleLikeBtnState();
   }
-  
-  _setEventListeners() {
-    this._card
-      .querySelector('.cards__trash-btn')
-      .addEventListener('click', () => this._handleCardDelete());
-    this._card
-      .querySelector('.cards__like-btn')
-      .addEventListener('click', () => this._handleLikeClick());
-    this._card.querySelector('.cards__image').addEventListener('click', () => {
-      this._handleCardClick({ name: this._name, link: this._link });
-    });
-  }
-
-  generateCard() {
-    this._card = this._getTemplate();
-    this._cardImage = this._card.querySelector('.cards__image');
-    this._cardHeading = this._card.querySelector('.cards__heading');
-    this._likeCounter = this._card.querySelector('.cards__like-counter');
-    this._likeBtn = this._card.querySelector('.cards__like-btn');
-    this._spinner = this._card.querySelector('.cards__spinner');
-    const imageLoader = new ImageLoader(
-      this._cardImage,
-      this._spinner,
-      'cards__image_error'
-    );
-    this._cardImage.src = this._link;
-    this._cardImage.alt = this._name;
-    this._cardHeading.textContent = this._name;
-    this._likeCounter.textContent = this._likes.length;
-    this._setTrashBtnState();
-    this._initializeLikeState();
-    this._setEventListeners();
-    imageLoader.initialize();
-    return this._card;
-  }
-
-  deleteCard() {
-    this._card.remove();
-    this._card = null;
-  }
-}
+  */
+//**********************************************************************************************************************************************
+//**********************************************************************************************************************************************
+//**********************************************************************************************************************************************
+//**********************************************************************************************************************************************
